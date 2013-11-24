b695ed674e8022b5c5d175deb0f2395fa85508e8
LayerZeroLabs <info@l0l.org.uk>
2013-11-24 12:40:30 +0000

    Changed directory structure
    in order to keep separate from node-red install dir now as new version allows

---
+++ nodes/99-pause.js
@@ -0,0 +1,17 @@
+// Simple node to introduce a pause into a flow
+ 
+//Require main module
+var RED = require(process.env.NODE_RED_HOME+"/red/red");
+
+function PauseNode(n) {
+   RED.nodes.createNode(this,n);
+ 
+   this.timeout = n.timeout * 1000;
+ 
+   this.on("input", function(msg) {
+       var node= this;
+       setTimeout(function(){node.send(msg);}, node.timeout);
+   });
+}
+ 
+RED.nodes.registerType("pause",PauseNode);
